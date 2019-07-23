00807fa146473505a9225e764f75152607ddfdfd
drab3n <32584507+drab3n@users.noreply.github.com>
2019-07-22 23:55:24 -0600

    Trove Polish, CSS Cleanup, img cleanup, Trove js fix

--- js/initializeTroveInfo.js
+++ js/initializeTroveInfo.js
@@ -1,17 +1,24 @@
 function initializeTrove() {
-    var list = document.getElementById("project-list");
-    var items = list.getElementsByTagName("li");
+    let list = document.getElementById("project-list");
+    let items = list.getElementsByTagName("li");
 
-    for (var i = 0; i < items.length; i++) {
-        var project = items[i];
-        var link = project.getElementsByTagName("a");
+    for (let i = 0; i < items.length; i++) {
+        let project = items[i];
+
+        // Returns a single-element list of all the a tags in this li (only one)
+        let link = project.getElementsByTagName("a");
+
+        // Because it's a list, need to index the only a tag in it
         link[0].onclick = function(){ loadProject(project.getAttribute("id"))};
-        link[0].href = "";
+        link[0].href = "#";
     }
 }
 
 function loadProject(projectName) {
-    var projectWindow = document.getElementById("projectWindow");
-    var projectFileName = projectName + ".html";
-    projectWindow.innerHTML= '<object data="'+ projectFileName+ '" />';
+
+    // TODO - Create quick and minimal md to HTML parser?
+
+    let projectWindow = document.getElementById("projectWindow");
+    let projectFileName = "trove/" + projectName + ".html";
+    projectWindow.innerHTML = '<object data="' +projectFileName+ '" />';
 }
