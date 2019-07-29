0195d74b2b38a0a2109ff92b9a1dae2db9e88978
Braden <drab3n@gmail.com>
2019-07-29 15:29:07 -0600

    Added some icons, "Current" polish

--- js/initializeTroveInfo.js
+++ js/initializeTroveInfo.js
@@ -5,39 +5,49 @@ function initializeTrove() {
     for (let i = 0; i < lists.length; i++) {
 
         let projectList = lists[i].getElementsByTagName("li");
+        let listID = lists[i].getAttribute("id");
 
         for (let j = 0; j < projectList.length; j++) {
             let project = projectList[j];
 
             // Returns a single-element list of all the a tags in this li (only one)
             let link = project.getElementsByTagName("a")[0];
-            let id = project.getAttribute("id");
+            let projectID = project.getAttribute("id");
 
-            link.innerHTML = id;
+            link.innerHTML = projectID;
 
             // Set the a tag properties
             link.onclick = function () {
-                loadProject(id)
+                loadProject(listID, projectID)
             };
             link.href = "#";
         }
     }
 }
 
-function loadProject(projectName) {
+function loadProject(listID, projectName) {
     let projectWindow = document.getElementById("projectWindow");
     let projectFileName = "doc/trove/" + projectName + ".html";
     projectWindow.innerHTML = '<object data="'+ projectFileName +'" />';
 }
 
-function createGithubLink() {
+function createGithubLink(inTrove) {
     let linkDiv = document.getElementById("gitLink");
     let link = linkDiv.getElementsByTagName("a")[0];
-    link.href = "https://github.com/bradendubois/trove/blob/master/" + linkDiv.getAttribute("title");
+
+    if (inTrove) {
+        link.href = "https://github.com/bradendubois/trove/blob/master/" + linkDiv.getAttribute("title");
+    } else  {
+        link.href = "https://github.com/bradendubois/" + linkDiv.getAttribute("title");
+    }
+
     link.target = "_blank";
 }
 
 function initializeTitle() {
     let titleTag = document.getElementById("projectTitle");
-    titleTag.innerHTML = document.getElementById("gitLink").getAttribute("title");
+
+    if (titleTag.innerHTML.length === 0) {
+        titleTag.innerHTML = document.getElementById("gitLink").getAttribute("title");
+    }
 }
